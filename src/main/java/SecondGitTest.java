658572ed7f280c5efe23ad2e4236dd10c51fef2a
Alexey <a.d.kamenev@yandex.ru>
2022-06-06 09:55:46 +0300

    Homework8 0.6 в новую ветку добавлен вызов метода

--- src/main/java/SecondGitTest.java
+++ src/main/java/SecondGitTest.java
@@ -2,6 +2,7 @@ public class SecondGitTest {
     public static void main(String[] args) {
         System.out.println("Новая ветка");
         // тест версии с новой веткой
+        info();
     }
     static void info(){
         System.out.println("Ветка сделана для работы с другого ПК");
