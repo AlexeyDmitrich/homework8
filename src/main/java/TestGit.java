e9f9b4c53223b09552e32843d4df52fbf0393f97
Alexey <a.d.kamenev@yandex.ru>
2022-06-06 09:10:20 +0300

    Homework8 0.1 git-test 1

---
+++ src/main/java/TestGit.java
@@ -0,0 +1,6 @@
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.print("Класс создан исключительно для тренировки работы в git \n");
+        // стартовая версия
+    }
+}
