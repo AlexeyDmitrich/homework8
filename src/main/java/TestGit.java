9394dc9b5acc3227b5e9eb781dc39f2a4a083c08
Alexey <a.d.kamenev@yandex.ru>
2022-06-06 09:21:12 +0300

    Homework8 0.3 добавлен вызов метода

--- src/main/java/TestGit.java
+++ src/main/java/TestGit.java
@@ -2,6 +2,8 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.print("Класс создан исключительно для тренировки работы в git \n");
         // стартовая версия
+        printTestText();
+        // третья версия
     }
     public static void printTestText(){
         System.out.println("Вторая версия");
