9ef910e48a50777a506f5e255dd0b04c05fe8741
Alexey <a.d.kamenev@yandex.ru>
2022-06-07 09:15:48 +0300

    homework8 1.1 добавлены наброски для реализации прыжков по дефолту

--- src/main/java/Cat.java
+++ src/main/java/Cat.java
@@ -19,7 +19,7 @@ public class Cat implements motion{
     @Override
     public void run(int distance) {
         if (distance > 0 && distance <= 150){
-            System.out.printf("%s пробежал %d метров");
+            System.out.printf("%s пробежал %d метров", name, distance);
         } else if (distance > 150){
             // todo: внести реализацию
         }
@@ -28,6 +28,16 @@ public class Cat implements motion{
 
     @Override
     public void jump(int high) {
-
+        if (high > 0 && high <= 2){
+            System.out.printf("%s перепрыгнул %d-метровую преграду", name, high);
+        } else if (high > 2 && high <= 4){
+            System.out.printf("%s смог перелезть через %d-метровую стену", name, high);
+        } else if (high > 4) {
+            System.out.printf("%s не смог преодолеть %d-метровое препятствие", name, high);
+        } else if (high == 0) {
+            System.out.printf("%s не заметил препятствия на пути", name);
+        } else {
+            System.out.printf("%s свалился в яму, глубиной -(%d метра) и еле выбрался", name, high);
+        }
     }
 }
--- src/main/java/motion.java
+++ src/main/java/motion.java
@@ -1,4 +1,17 @@
 public interface motion {
     void run (int distance);
-    void jump (int high);
+
+    default void jump(int high, String name, int barier, int limit) {
+        if (high > 0 && high <= barier){
+            System.out.printf("%s перепрыгнул %d-метровую преграду", name, high);
+        } else if (high > barier && high <= limit){
+            System.out.printf("%s смог перелезть через %d-метровую стену", name, high);
+        } else if (high > limit) {
+            System.out.printf("%s не смог преодолеть %d-метровое препятствие", name, high);
+        } else if (high == 0) {
+            System.out.printf("%s не заметил препятствия на пути", name);
+        } else {
+            System.out.printf("%s свалился в яму, глубиной -(%d метра) и еле выбрался", name, high);
+        }
+    }
 }
