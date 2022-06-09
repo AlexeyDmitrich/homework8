d015216678ab22a5d7d9c81b1161fc2b46c7f7d6
Alexey <a.d.kamenev@yandex.ru>
2022-06-09 07:31:23 +0300

    homework8 1.2.1 добавлены классы для стены и дороги

--- src/main/java/MainHomework8.java
+++ src/main/java/MainHomework8.java
@@ -1,5 +1,6 @@
 public class MainHomework8 {
     public static void main(String[] args) {
-
+Cat koks = new Cat("Koks", 5);
+koks.catRun(100);
     }
 }
---
+++ src/main/java/Wall.java
@@ -0,0 +1,11 @@
+public class Wall {
+    double high;
+
+    public Wall(double high) {
+        this.high = high;
+    }
+
+    public double getHigh() {
+        return high;
+    }
+}
